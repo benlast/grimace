a5612fe879c729e25b4b3cd51feb9bd9a92966c4
Nando Florestan <nandoflorestan@gmail.com>
2013-07-31 09:42:23 -0300

    Fix Python packaging.
    The current version cannot be easy_installed. It says:
    error: Couldn't find a setup script in /tmp/easy_install-LmAZGR/grimace-0.0.10.linux-x86_64.tar.gz
    
    In the future, do not create binary distributions. Use only sdist

---
+++ README.rst
@@ -0,0 +1,9 @@
+*grimace* is a fluent regular expression string builder for Python.
+Inspired by Al Williams' article in Dr Dobbs:
+http://www.drdobbs.com/embedded-systems/irregular-expressions/240157655#
+
+* Documentation_
+* `Project home`_
+
+.. _Documentation: https://github.com/benlast/grimace/wiki
+.. _`Project home`_: https://github.com/benlast/grimace
--- setup.py
+++ setup.py
@@ -1,34 +1,54 @@
-__author__ = 'benlast <ben@benlast.com>'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from __future__ import (absolute_import, division, print_function)
+import codecs
+from os import path
+from setuptools import setup, find_packages
 from _version import __version__
-import sys
 
 
-classifiers = """\
-Development Status :: 4 - Beta
-Intended Audience :: Developers
-License :: OSI Approved :: MIT License
-License :: OSI Approved :: Academic Free License (AFL)
-Programming Language :: Python
-Topic :: Software Development :: Code Generators
-Topic :: Software Development :: Libraries :: Python Modules
-Operating System :: Microsoft :: Windows
-Operating System :: Unix
-"""
+def content_of(paths, encoding='utf-8', sep='\n'):
+    if isinstance(paths, str):
+        paths = [paths]
+    content = []
+    for path in paths:
+        with codecs.open(path, encoding=encoding) as stream:
+            content.append(stream.read())
+    return sep.join(content)
 
-kwargs = {
-    'name': 'grimace',
-    'version': __version__,
-    'description': 'A fluent regular expression generator',
-    'author': 'Ben Last',
-    'author_email': 'ben@benlast.com',
-    'url': 'https://github.com/benlast/grimace/wiki',
-    'license': 'MIT, Academic Free License version 2.1',
-    'packages': ['grimace'],
-}
 
-if sys.version >= (2, 3):
-    kwargs['classifiers'] = classifiers.split('\n')
+here = path.abspath(path.dirname(__file__))
 
-setup(**kwargs)
+setup(name='grimace',
+      version=__version__,
+      description='A fluent regular expression generator',
+      long_description=content_of(path.join(here, 'README.rst')),
+      author='Ben Last',
+      author_email='ben@benlast.com',
+      url='https://github.com/benlast/grimace',
+      license='MIT, Academic Free License version 2.1',
+      classifiers=[  # http://pypi.python.org/pypi?:action=list_classifiers
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: Academic Free License (AFL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Software Development :: Code Generators',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords=['re', 'regex', 'regexp', 'regular expression', 'fluent'],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      # install_requires=['nine'],
+      test_suite='grimace.tests',
+)
